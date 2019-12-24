fdff5a4c4805f268d418056dde9a8dd0bcf53313
Alexey Lebedev <vorresti@ya.ru>
2019-12-24 17:51:52 +0300

    HW_2_v1.1: menu and slider modified

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,27 +1,26 @@
-let mainMenu = Array.from(document.querySelectorAll('ul.menu_main > li > a'));
+const mainMenu = Array.from(document.querySelectorAll('ul.menu_main > li > a'));
+const subMenu = Array.from(document.querySelectorAll('.menu_sub'));
 
 for (mainMenuItem of mainMenu) {
+
     mainMenuItem.onclick = function() {
 
-        //mainMenu.forEach(menuElement => menuElement.nextElementSibling.classList.remove('menu_active'));
-        //этой строчкой я хотел у всех элементов удалить активное состояние, чтобы даже при нажатии на пункт меню, который
-        //не содержит подменю, закрылось любое открытое меню и после этого состоялся переход по ссылке.
-        //идея была в том, чтобы при каждом клике сперва закрывать меню, а потом уже открывать нужное или переходить по ссылке в пунктах без подменю.
-        //но при таком исполнении страничка обновляется - код после строки 6 не доходит до условий и не выполняется return false
-        //я рассчитывал на то, что строка 6 закроет любое активное подменю и далее состоится проверка в строке 13
-        
         if (this.nextElementSibling.classList.contains('menu_sub')) {
 
             if (this.nextElementSibling.classList.contains('menu_active')) {
                 this.nextElementSibling.classList.remove('menu_active');
             } else {
+                
+                for (subMenuItem of subMenu) {
+                    if (subMenuItem.classList.contains('menu_active')) {
+                        subMenuItem.classList.remove('menu_active');
+                    }
+                }
+
                 this.nextElementSibling.classList.add('menu_active');
             }
             
-            return false;
+        return false;
         }
-
-        
     };
-    //console.log('click added');
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,27 +5,51 @@ const dots = Array.from(document.getElementsByClassName('slider__dot'));
 
 let currentSlide = 0;
 
+function clearDotsIndicators() {
+    for (dot of dots) {
+        if (dot.classList.contains('slider__dot_active')) {
+            dot.classList.remove('slider__dot_active');
+        }
+    }
+};
+
+function setDotIndicator(dotNumber) {
+    dots[dotNumber].classList.add('slider__dot_active');
+}
+
+setDotIndicator(currentSlide);
+
 prevButton.onclick = function() {
     if (currentSlide === 0) {
+        
         sliders[currentSlide].classList.remove('slider__item_active');
-        sliders[sliders.length - 1].classList.add('slider__item_active');
         currentSlide = sliders.length - 1;
+        sliders[currentSlide].classList.add('slider__item_active');
+        clearDotsIndicators();
+        setDotIndicator(currentSlide);
+
     } else {
         sliders[currentSlide].classList.remove('slider__item_active');
-        sliders[currentSlide - 1].classList.add('slider__item_active');
         currentSlide--;
+        sliders[currentSlide].classList.add('slider__item_active');
+        clearDotsIndicators();
+        setDotIndicator(currentSlide);
     }
 };
 
 nextButton.onclick = function() {
     if (currentSlide === sliders.length - 1) {
         sliders[currentSlide].classList.remove('slider__item_active');
-        sliders[0].classList.add('slider__item_active');
         currentSlide = 0;
+        sliders[currentSlide].classList.add('slider__item_active');
+        clearDotsIndicators();
+        setDotIndicator(currentSlide);
     } else {
         sliders[currentSlide].classList.remove('slider__item_active');
-        sliders[currentSlide + 1].classList.add('slider__item_active');
         currentSlide++;
+        sliders[currentSlide].classList.add('slider__item_active');
+        clearDotsIndicators();
+        setDotIndicator(currentSlide);
     }
 };
 
@@ -34,5 +58,7 @@ dots.forEach((dot, i) => {
         sliders[currentSlide].classList.remove('slider__item_active');
         currentSlide = i;
         sliders[currentSlide].classList.add('slider__item_active');
+        clearDotsIndicators();
+        setDotIndicator(currentSlide);
     }
 });
