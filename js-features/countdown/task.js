ea3665a21ab78b479b8bc8d7982a0c29bbdd7f07
Alexey Lebedev <vorresti@ya.ru>
2019-12-25 17:39:23 +0300

    coundown timer fixed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,16 +1,46 @@
-const timerEl = document.getElementById('timer');
-const timerText = document.getElementById('timerText');
-let count = document.getElementById('timer').textContent;
+const countdownTimer = function() {
+    const timer = document.getElementById('timer');
 
-const timer = setInterval(() => {
-  if (timerEl.textContent < 11) {
-      timerText.textContent = 'До окончания конкурса осталось секунд: 0';
-  }
+    const timerElements = timer.textContent;
+    const arr = timerElements.split(':');   
+    let hour = arr[0];
+    let min = arr[1];
+    let sec = arr[2];    
 
-    timerEl.textContent -= 1;
-    count--;
-    if (count === 0) {
-        clearInterval(timer);
-        alert('Вы победили в конкурсе!');
+    if (sec == 0) {
+        if (min == 0) {
+            if (hour == 0) {
+                clearInterval(сount);        
+                alert('Вы победили в конкурсе!');
+                return;
+            }
+            
+            hour--;
+            min = 60;
+            sec = 60;
+
+            if (hour < 10) {
+                hour = '0' + hour;
+            }
+        }
+
+        min--;
+        sec = 60;
+
+        if (min < 10) {
+            min = '0' + min;
+        }
     }
-}, 1000);
+
+    sec--;
+
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+
+    currentTime = `${hour}:${min}:${sec}`;
+
+    document.getElementById('timer').innerHTML = currentTime;       
+}
+
+const сount = setInterval(countdownTimer, 1000);
