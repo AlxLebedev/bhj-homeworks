a8fd7f50270def4a8787eb3d5bd57f57e72f91b4
Alexey Lebedev <vorresti@ya.ru>
2020-01-14 19:32:48 +0300

    reveral fixed

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,29 +1,11 @@
-const reverals = Array.from(document.querySelectorAll('.reveal'));
-let currentReveral = 0;
+const reveals = Array.from(document.querySelectorAll('.reveal'));
 
-function showIfInViewPort(element) {
-    
-    const viewPortHeight = window.innerHeight;
-    const elementLocation = element.getBoundingClientRect();
-    let onViewPort = false;
-
-    if (elementLocation.top >= 0 || elementLocation.bottom >= 0) {
-        onViewPort = elementLocation.top < viewPortHeight ? true : false;
-    }
-    
-    if (onViewPort) {
-        element.classList.add('reveal_active');
-    } else {
-        element.classList.remove('reveal_active');
-    }
-
-    if (reverals[0].getBoundingClientRect().top < (-viewPortHeight)) { //не очень-то мне нравится мой способ смены элемента. А если их на странице 33 штуки как быть?
-        currentReveral = 1;
-    } else {
-        currentReveral = 0;
-    }
-}
-
-window.onscroll = function() {
-    showIfInViewPort(reverals[currentReveral]);
-}
+document.addEventListener('scroll', function() {
+    reveals.forEach(revealElement => {
+        if (window.innerHeight > revealElement.getBoundingClientRect().top && revealElement.getBoundingClientRect().bottom > 0) {
+            revealElement.classList.add('reveal_active');
+        } else {
+            revealElement.classList.remove('reveal_active');
+        }
+    });
+});
